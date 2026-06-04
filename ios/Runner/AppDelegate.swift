e162614a69385f8a97935f02d3c760743681b651
wescley_pugliese <wescley.pugliese@gmail.com>
2026-06-04 13:44:05 -0300

    Implementacao do Google maps e autorização

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,16 +1,15 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps // <-- ADICIONE ESTA LINHA
 
-@main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("SUA_CHAVE_AQUI") // <-- ADICIONE ESTA LINHA
+    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
-    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
